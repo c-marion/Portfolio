af0329024593946f10f6a67aa234b9653b20f907
c-marion <2168153@johnabbottcollege.net>
2023-11-13 17:21:22 -0500

    added documentation and some sample lorem ipsum text

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,10 +1,15 @@
 import "../App.css";
+import LoremIpsum from "./LoremIpsum";
 
+/**
+ * The container with all of the professional information about me (Caden).
+ */
 export default function Portfolio() {
     return (
         <div className="inner-container" id="portfolio-container">
             <h2 className="portfolio-header">Test</h2>
             <hr/>
+            <LoremIpsum/>
         </div>
     )
 }
--- src/Components/SelfDescription.js
+++ src/Components/SelfDescription.js
@@ -1,9 +1,14 @@
 import "../App.css";
+import LoremIpsum from "./LoremIpsum";
 
+/**
+ * The container with all of the personal (to an extent) information about me (Caden).
+ */
 export default function SelfDescription() {
     return (
         <div className="inner-container" id="self-description">
             <h2 className="portfolio-header">Self</h2>
+            <LoremIpsum/>
         </div>
     )
 }
