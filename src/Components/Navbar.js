37016f861da5c06a4c51ddfd548934ca06d39970
c-marion <2168153@johnabbottcollege.net>
2023-11-20 08:03:47 -0500

    added an automatic link generator

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,13 @@
 
 
-export default function Navbar() {
+export default function Navbar(params) {
     return (
         <div className="navbar">
-
+            {
+                params && params.link && params.links.map((link, index) => {
+                    return <a href={`#${link.addressId}`}>{link.name}</a>
+                })
+            }
         </div>
     );
 }
