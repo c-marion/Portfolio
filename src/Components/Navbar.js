68219847e276a6de92e34393fcbbce611e805569
c-marion <2168153@johnabbottcollege.net>
2023-11-20 09:17:05 -0500

    added documentation

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,9 @@
 
 
+/**
+ * A navigation bar that sends the user to different parts of the page.
+ * @param {Object} params Parameters for the react component. To add navbar items, put a links array property of { name, addressId } objects.
+ */
 export default function Navbar(params) {
     return (
         <div className="navbar">
