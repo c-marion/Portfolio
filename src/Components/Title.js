6f2bcf5ea694b611c2a640ea6fe01f491ef360fb
c-marion <2168153@johnabbottcollege.net>
2023-11-22 10:02:54 -0500

    working on the real descriptions

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,4 +1,4 @@
-import LoremIpsum from "./LoremIpsum";
+
 
 /**
  * The first component you see on the website, complete with a name, personal description, and more.
@@ -7,7 +7,7 @@ export default function Title() {
     return (
         <div className="title-container">
             <h1 className="title-heading">Caden Marion</h1>
-            <h3 className="title-subheading"><LoremIpsum/></h3>
+            <h3 className="title-subheading">Full time student and John Abbott College</h3>
         </div>
     )
 }
