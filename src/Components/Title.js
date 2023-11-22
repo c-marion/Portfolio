43dd586a8d0dea0188b735c6115c229c3816b650
c-marion <2168153@johnabbottcollege.net>
2023-11-22 09:26:21 -0500

    added documentation

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,9 +1,12 @@
 
 
+/**
+ * The first component you see on the website, complete with a name, personal description, and more.
+ */
 export default function Title() {
     return (
         <div className="title-container">
-            
+
         </div>
     )
 }
