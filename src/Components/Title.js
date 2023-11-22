d6b780778b89f5fdfaaf94dbc87d00afd3dd2571
c-marion <2168153@johnabbottcollege.net>
2023-11-22 10:00:59 -0500

    working on the title and subtitle

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,4 +1,4 @@
-
+import LoremIpsum from "./LoremIpsum";
 
 /**
  * The first component you see on the website, complete with a name, personal description, and more.
@@ -6,7 +6,8 @@
 export default function Title() {
     return (
         <div className="title-container">
-            <h1>Test</h1>
+            <h1 className="title-heading">Caden Marion</h1>
+            <h3 className="title-subheading"><LoremIpsum/></h3>
         </div>
     )
 }
