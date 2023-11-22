5fa04f31dcacc96d2fa8431088046d5e37a49449
c-marion <2168153@johnabbottcollege.net>
2023-11-22 15:26:45 -0500

    working on the about me text

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -2,8 +2,11 @@
 
 export default function Aboutme() {
     return (
-        <div className="aboutme-container">
-            
+        <div className="aboutme-container" id="about">
+            <h1 className="aboutme-heading">About Me</h1>
+            <p className="aboutme-paragraph">
+                My name is Caden Marion
+            </p>
         </div>
     )
 }
