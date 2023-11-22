cc58a52779916cf2aea2096e2bf876a0d6f2f577
c-marion <2168153@johnabbottcollege.net>
2023-11-22 18:32:37 -0500

    writing some text

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -8,7 +8,7 @@ export default function Aboutme() {
             <div className="aboutme-paragraph-container">
                 <h2 className="aboutme-heading">About Me</h2>
                 <p className="aboutme-paragraph">
-                    My name is Caden Marion
+                    My name is Caden Marion. I'm a computer science student at John Abbott College
                 </p>
             </div>
             
