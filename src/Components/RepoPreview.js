78cee6b734f6e0d2d7f9fb893d256a4ba3576899
c-marion <2168153@johnabbottcollege.net>
2023-11-23 17:49:40 -0500

    working on filling out each repo

--- src/Components/RepoPreview.js
+++ src/Components/RepoPreview.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 
 
 export default function RepoPreview(params) {
+    const [languages, setLanguages] = useState([]);
+    const [languageTotal, setLanguageTotal] = useState(0);
+
+    useEffect(function () {
+        fetch("https://api.github.com/repos/c-marion/Portfolio/languages")
+        .then(function (response) {
+            response.json()
+            .then(function (languages) {
+                setLanguages(languages);
+
+                const keys = Object.keys(languages);
+                let total = 0;
+                
+                for (let i = 0; i < keys.length; i++) {
+                    total += languages[keys[i]];
+                }
+
+                setLanguageTotal(total);
+            })
+        })
+    }, []);
+
     return (
         <div className="repo-container">
-
+            <h4 className="repo-header">{params.repo && params.repo.name}</h4>
+            <hr/>
+            <p>{languageTotal && languageTotal}</p>
         </div>
     );
 }
