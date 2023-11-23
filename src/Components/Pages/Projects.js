0c40b89c9483a983db74c2ab43931362ca8724cf
c-marion <2168153@johnabbottcollege.net>
2023-11-23 17:04:55 -0500

    adding a projects list

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import RepoPreview from "../RepoPreview";
 
 
 export default function Projects() {
@@ -7,8 +8,8 @@ export default function Projects() {
         const response = fetch("https://api.github.com/users/c-marion/repos")
         .then(function(response) {
             response.json()
-            .then(function(response) {
-                console.log(response);
+            .then(function(repos) {
+                setRepos(repos);
             });
         });
     }, []);
@@ -17,6 +18,12 @@ export default function Projects() {
         <div className="projects-container" id="projects">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <h1>Projects</h1>
+
+            <ul>
+                <li className="projects-li">
+                    <RepoPreview repo={repos[0]}/>
+                </li>
+            </ul>
             
         </div>
     );
