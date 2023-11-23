11241399a9c6a4943afcbdcfbb4eb0f682547494
c-marion <2168153@johnabbottcollege.net>
2023-11-23 10:40:08 -0500

    working on filling this out

--- src/Components/Pages/Skills.js
+++ src/Components/Pages/Skills.js
@@ -2,8 +2,17 @@
 
 export default function Skills() {
     return (
-        <div className="skills-container">
+        <div className="skills-container" id="skills">
+            <link rel="stylesheet"href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.10.1/devicon.min.css"/>
             <h1 className="aboutme-heading">Skills</h1>
+
+            <ul>
+                <li className="icon-btn">
+                    <i className="devicon-javascript-plain"></i>
+                </li>
+            </ul>
+            
+            
         </div>
     )
 }
