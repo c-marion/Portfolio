2991756e833e6d07dacf93e4ac8b2f7fb0585ee8
c-marion <2168153@johnabbottcollege.net>
2023-11-23 18:52:27 -0500

    added some more repo details

--- src/Components/RepoPreview.js
+++ src/Components/RepoPreview.js
@@ -37,15 +37,17 @@ export default function RepoPreview(params) {
 
     return (
         <div className="repo-container">
-            <h4 className="repo-header">{params.repo && params.repo.name}</h4>
-            <p className="repo-desc">{params.repo && params.repo.description}</p>
+            <div className="repo-inner-container">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                <h4 className="repo-header">{params.repo && params.repo.name}</h4>
+                <p className="repo-desc">{params.repo && params.repo.description}</p>
 
-            <button className="repo-btn">
-                <a className="repo-link" href={params.repo && params.repo.clone_url}>Clone Repo</a>
-            </button>
+                <button className="repo-btn">
+                    <a className="repo-link" href={params.repo && params.repo.clone_url}><i className="fa fa-github"></i> Repository</a>
+                </button>
+            </div>
             
             <hr/>
-            
             <div className="language-container">
             {
                 languages && getLanguageAmountList().map((langAmount, index) => {
