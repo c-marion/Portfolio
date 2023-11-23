ef1d904c9c0ff6cde2fbc70b954d9411f0d7c1ef
c-marion <2168153@johnabbottcollege.net>
2023-11-23 10:53:18 -0500

    working on skills list

--- src/Components/Pages/Skills.js
+++ src/Components/Pages/Skills.js
@@ -5,9 +5,9 @@ export default function Skills() {
         <div className="skills-container" id="skills">
             <link rel="stylesheet"href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.10.1/devicon.min.css"/>
             <h1 className="aboutme-heading">Skills</h1>
-
-            <ul>
-                <li className="icon-btn">
+            
+            <ul className="skills-list">
+                <li className="skills-icon">
                     <i className="devicon-javascript-plain"></i>
                 </li>
             </ul>
