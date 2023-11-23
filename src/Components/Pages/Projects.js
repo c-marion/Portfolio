ca66071d76e13ee32e47605df19aa451b66e5d36
c-marion <2168153@johnabbottcollege.net>
2023-11-23 16:53:30 -0500

    working on showing github repositories

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 
 
 export default function Projects() {
+    const [repos, setRepos] = useState([]);
+    const user = useEffect(function() {
+        const response = fetch("https://api.github.com/users/c-marion/repos")
+        .then(function(response) {
+            response.json()
+            .then(function(response) {
+                console.log(response);
+            });
+        });
+    }, []);
+
     return (
         <div className="projects-container" id="projects">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <h1>Projects</h1>
+            
         </div>
     );
 }
