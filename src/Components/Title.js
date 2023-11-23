e65c2fe2f5274363966b953f2174530555179052
c-marion <2168153@johnabbottcollege.net>
2023-11-22 19:21:43 -0500

    added a github button

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -11,10 +11,16 @@ export default function Title() {
             <div className="title-circle" id="title-circle2"/>
 
             <h1 className="title-heading">Caden Marion</h1>
-            <h3 className="title-subheading">Full time Computer Science student and John Abbott College.</h3>
-            <button className="title-btn">
-                <a className="title-link" role="button" target="_target" href="https://drive.google.com/file/d/1q9RQ0rY-3iYLgGN9kBx-qPU_OKRTyrzO/view?usp=sharing"><b>RESUME</b></a>
-            </button>
+            <h3 className="title-subheading">Full time Computer Science student and John Abbott College. </h3>
+
+            <div className="title-btns">
+                <button className="title-btn">
+                    <a className="title-link" role="button" rel="noopener noreferrer" target="_target" href="https://drive.google.com/file/d/1q9RQ0rY-3iYLgGN9kBx-qPU_OKRTyrzO/view?usp=sharing"><b>RESUME</b></a>
+                </button>
+                <button className="title-btn">
+                    <a className="title-link" role="button" rel="noopener noreferrer" target="_target" href="https://github.com/c-marion"><b>GITHUB</b></a>
+                </button>
+            </div>
             
         </div>
         </>
