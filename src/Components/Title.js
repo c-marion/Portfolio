fa6b001861ddf8e1d9aa9c6eb96323bc2b263b43
c-marion <2168153@johnabbottcollege.net>
2023-11-22 19:00:32 -0500

    added a link to my resume/cv

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -12,6 +12,10 @@ export default function Title() {
 
             <h1 className="title-heading">Caden Marion</h1>
             <h3 className="title-subheading">Full time Computer Science student and John Abbott College.</h3>
+            <button className="title-btn">
+                <a className="title-link" role="button" target="_target" href="https://drive.google.com/file/d/1q9RQ0rY-3iYLgGN9kBx-qPU_OKRTyrzO/view?usp=sharing"><b>RESUME</b></a>
+            </button>
+            
         </div>
         </>
     )
