78bdd8090075830ff813f26d8d8b26e7906080e9
c-marion <2168153@johnabbottcollege.net>
2023-11-23 19:51:01 -0500

    replaced the fancy title screen effects with a simple one

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -7,9 +7,6 @@ export default function Title() {
     return (
         <>
         <div className="title-container" id="title">
-            <div className="title-circle" id="title-circle1"/>
-            <div className="title-circle" id="title-circle2"/>
-
             <h1 className="title-heading">Caden Marion</h1>
             <h3 className="title-subheading">Full time Computer Science student and John Abbott College. </h3>
 
